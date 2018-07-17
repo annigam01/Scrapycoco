90c6709965ea85f2d5f5ce9baa412ffaed0d7641
annigam01 <annigam01@hotmail.com>
2018-07-17 15:53:57 +1000

    it downloads AIP client from internet to(c:\windows\temp\) and install the AIP client

---
+++ install-AIP.ps1
@@ -0,0 +1,5 @@
+﻿$url = "https://download.microsoft.com/download/4/9/1/491251F7-46BA-46EC-B2B5-099155DD3C27/AZInfoProtection_MSI_for_central_deployment.msi"
+$outpath = "c:\windows\temp\aip.msi"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+Start-Process -Filepath $outpath
